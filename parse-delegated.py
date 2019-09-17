82661530d046e821944a6770b4fd0a59012a2960
mrizvic <marko.rizvic@gmail.com>
2019-09-17 21:15:33 +0200

    parse asn and create asnXXXX.ipv(4|6).txt files

--- parse-delegated.py
+++ parse-delegated.py
@@ -44,33 +44,68 @@ numcidr4={
 }
 
 allocs = dict()
+asnv4 = dict()
+asnv6 = dict()
+asnext = dict()
 
 ### HELPER
-def addKeyVal (key, value):
-	try: allocs[key]
-	except KeyError: allocs[key] = list()
-	finally: allocs[key].append(value)
+def addKeyVal (dictionary, key, value):
+	try: dictionary[key]
+	except KeyError: dictionary[key] = list()
+	finally: dictionary[key].append(value)
 	return
 
 for line in sys.stdin.readlines():
+	line = line.rstrip()
 
 	### FOR EACH allocated ipv4
         if line.find("|ipv4") > -1 and line.find("allocated",35) > 0:
                 line = line.rstrip()
-                (ripe,cc,addr_family,netaddr,num,date,status,id) = line.split('|')
+                (registry,cc,addr_family,netaddr,num,date,status,extension) = line.split('|')
                 cidr = numcidr4[num]
 		key = "{0}-ipv4".format(cc)
 		value = "{0}/{1}".format(netaddr, cidr)
-		addKeyVal(key, value)
+		addKeyVal(allocs, key, value)
+		addKeyVal(asnv4, extension, value)
 		continue
 
 	### FOR EACH allocated ipv6
         if line.find("|ipv6") > -1 and line.find("allocated",35) > 0:
-                (ripe,cc,addr_family,netaddr,cidr,date,status,id) = line.split('|')
+                (registry,cc,addr_family,netaddr,cidr,date,status,extension) = line.split('|')
 		key = "{0}-ipv6".format(cc)
 		value = "{0}/{1}".format(netaddr, cidr)
-		addKeyVal(key, value)
+		addKeyVal(allocs, key, value)
+		addKeyVal(asnv6, extension, value)
 		continue
+
+	### FOR EACH allocated ipv6
+	### ripencc|EU|asn|251|1|19930901|allocated|935422fc-24cc-4ad9-b447-32e3c258614a
+        if line.find("|asn") > -1 and line.find("allocated",25) > 0:
+		(registry,cc,rtype,asn,value,date,status,extension) = line.split('|')
+		addKeyVal(asnext, asn, extension)
+		continue
+		
+### CREATE FILE FOR EACH asn.family
+for asn in sorted(asnext):
+	ext = asnext[asn][0]
+
+	fname = "asn{0}.ipv4.txt".format(asn)
+	fd = open(fname, 'w')
+	try:
+		for pool in asnv4[ext]:
+			fd.write("{0}\n".format(pool))
+	except Exception as e:
+			print(e)
+	fd.close()
+
+	fname = "asn{0}.ipv6.txt".format(asn)
+	fd = open(fname, 'w')
+	try:
+		for pool in asnv6[ext]:
+			fd.write("{0}\n".format(pool))
+	except Exception as e:
+			print(e)
+	fd.close()
 
 ### CREATE FILE FOR EACH KEY
 for key in sorted(allocs):
